f0ceadfa41584423221d778dd52c61e0e2383237
Theodore Yamit <116919917+TheoYamit@users.noreply.github.com>
2025-07-12 00:32:51 -0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/NewWebsitePortfolio">
         <Routes>
           <Route element={<Layout/>}>
             <Route path="/" element={<Home/>}/>
